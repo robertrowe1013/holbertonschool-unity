9e8307dbd1efdae29d5506dd13b307e6973e4a17
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-02-01 11:28:21 -0600

    timer trigger

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    public float timerValue = 0f;
+    public bool timerValueTrigger = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerValueTrigger)
+        {
+            timerValue += Time.deltaTime;
+            float minutes = Mathf.FloorToInt(timerValue / 60);
+            float seconds = Mathf.FloorToInt(timerValue % 60);
+            float milliseconds = (timerValue % 1) * 100;
+            TimerText.text = string.Format("{0}:{1:00}.{2:00}", minutes, seconds, milliseconds);
+        }
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    public GameObject player;
+
+    void OnTriggerExit()
+    {
+        player.GetComponent<Timer>().timerValueTrigger = true;
+    }
+}
