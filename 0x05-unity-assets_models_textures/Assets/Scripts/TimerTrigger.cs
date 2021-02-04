e77cbcb84646bdfefda1dd14125c7e920c09eccc
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-02-03 18:53:42 -0600

    coins

--- 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class TimerTrigger : MonoBehaviour
 {
     public GameObject player;
+    GameObject[] coins;
 
     void OnTriggerExit()
     {
+        player.GetComponent<Timer>().timerValue = 0f;
         player.GetComponent<Timer>().timerValueTrigger = true;
+        coins = GameObject.FindGameObjectsWithTag("Coin");
+        foreach (GameObject coin in coins)
+        {
+            coin.SetActive(true);
+        }
     }
 }
