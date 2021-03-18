b57fe1ba1e24ecd9ab28fa18f3887cd477ee37a7
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-18 14:58:19 -0500

    bgm and sfx sliders

--- 0x08-unity-audio/Assets/Scripts/PauseMenu.cs
+++ 0x08-unity-audio/Assets/Scripts/PauseMenu.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class PauseMenu : MonoBehaviour
 {
     public bool gameIsPaused = false;
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot unpaused;
 
     public void Pause()
     {
@@ -15,6 +18,7 @@ public class PauseMenu : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Time.timeScale = 0f;
         gameIsPaused = true;
+        paused.TransitionTo(.01f);
     }
 
     public void Resume()
@@ -24,6 +28,7 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         gameIsPaused = false;
         Cursor.visible = false;
+        unpaused.TransitionTo(.01f);
     }
 
     public void Restart()
@@ -33,6 +38,7 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         gameIsPaused = false;
         Cursor.visible = false;
+        unpaused.TransitionTo(.01f);
         string sceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(sceneName);
     }
@@ -42,6 +48,7 @@ public class PauseMenu : MonoBehaviour
         gameObject.SetActive(false);
         Time.timeScale = 1f;
         gameIsPaused = false;
+        unpaused.TransitionTo(.01f);
         SceneManager.LoadScene(0);
     }
 
@@ -50,6 +57,7 @@ public class PauseMenu : MonoBehaviour
         gameObject.SetActive(false);
         Time.timeScale = 1f;
         gameIsPaused = false;
+        unpaused.TransitionTo(.01f);
         PlayerPrefs.SetString("lastLoadedScene", SceneManager.GetActiveScene().name);
         SceneManager.LoadScene(4);
     }
