fb6990e2acc4b33f0530f25d9a32de12487c6194
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-08-12 17:25:33 -0500

    final

--- 0x09-unity_ar_business_card/ARBusinessCard/Assets/Scripts/DieManager.cs
+++ 0x09-unity_ar_business_card/ARBusinessCard/Assets/Scripts/DieManager.cs
@@ -25,12 +25,87 @@ public class DieManager : MonoBehaviour
         if (isSpinning == true)
         {
             isSpinning = false;
-            // anim.speed = 0;
+            anim.speed = 0;
         }
         else
         {
             isSpinning = true;
-            // anim.speed = 1;
+            anim.speed = 1;
+        }
+    }
+
+    public void GitClick()
+    {
+        if (isSpinning == true)
+        {
+            isSpinning = false;
+            anim.speed = 0;
+            Application.OpenURL("https://github.com/robertrowe1013");
+        }
+        else
+        {
+            isSpinning = true;
+            anim.speed = 1;
+        }
+    }
+
+    public void FaceClick()
+    {
+        if (isSpinning == true)
+        {
+            isSpinning = false;
+            anim.speed = 0;
+            Application.OpenURL("https://www.facebook.com/groups/2385572485046288/");
+        }
+        else
+        {
+            isSpinning = true;
+            anim.speed = 1;
+        }
+    }
+
+    public void LinkedClick()
+    {
+        if (isSpinning == true)
+        {
+            isSpinning = false;
+            anim.speed = 0;
+            Application.OpenURL("https://www.linkedin.com/in/robertrowe1013/");
+        }
+        else
+        {
+            isSpinning = true;
+            anim.speed = 1;
+        }
+    }
+
+    public void GmailClick()
+    {
+        if (isSpinning == true)
+        {
+            isSpinning = false;
+            anim.speed = 0;
+            Application.OpenURL("mailto:robertrowe1013@gmail.com");
+        }
+        else
+        {
+            isSpinning = true;
+            anim.speed = 1;
+        }
+    }
+
+    public void TwitterClick()
+    {
+        if (isSpinning == true)
+        {
+            isSpinning = false;
+            anim.speed = 0;
+            Application.OpenURL("https://twitter.com/RobertRowe1013");
+        }
+        else
+        {
+            isSpinning = true;
+            anim.speed = 1;
         }
     }
 }
