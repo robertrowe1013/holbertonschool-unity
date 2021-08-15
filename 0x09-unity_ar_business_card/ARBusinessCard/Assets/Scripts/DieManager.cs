6883b8c74826bcf3a967ad4bd4c44f73696cfa41
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-08-15 05:41:46 -0500

    fixed side clicks bug not opening when not spinning

--- 0x09-unity_ar_business_card/ARBusinessCard/Assets/Scripts/DieManager.cs
+++ 0x09-unity_ar_business_card/ARBusinessCard/Assets/Scripts/DieManager.cs
@@ -36,76 +36,31 @@ public class DieManager : MonoBehaviour
 
     public void GitClick()
     {
-        if (isSpinning == true)
-        {
-            isSpinning = false;
-            anim.speed = 0;
-            Application.OpenURL("https://github.com/robertrowe1013");
-        }
-        else
-        {
-            isSpinning = true;
-            anim.speed = 1;
-        }
+        TopClick();
+        Application.OpenURL("https://github.com/robertrowe1013");
     }
 
     public void FaceClick()
     {
-        if (isSpinning == true)
-        {
-            isSpinning = false;
-            anim.speed = 0;
-            Application.OpenURL("https://www.facebook.com/groups/2385572485046288/");
-        }
-        else
-        {
-            isSpinning = true;
-            anim.speed = 1;
-        }
+        TopClick();
+        Application.OpenURL("https://www.facebook.com/groups/2385572485046288/");
     }
 
     public void LinkedClick()
     {
-        if (isSpinning == true)
-        {
-            isSpinning = false;
-            anim.speed = 0;
-            Application.OpenURL("https://www.linkedin.com/in/robertrowe1013/");
-        }
-        else
-        {
-            isSpinning = true;
-            anim.speed = 1;
-        }
+        TopClick();
+        Application.OpenURL("https://www.linkedin.com/in/robertrowe1013/");
     }
 
     public void GmailClick()
     {
-        if (isSpinning == true)
-        {
-            isSpinning = false;
-            anim.speed = 0;
-            Application.OpenURL("mailto:robertrowe1013@gmail.com");
-        }
-        else
-        {
-            isSpinning = true;
-            anim.speed = 1;
-        }
+        TopClick();
+        Application.OpenURL("mailto:robertrowe1013@gmail.com");
     }
 
     public void TwitterClick()
     {
-        if (isSpinning == true)
-        {
-            isSpinning = false;
-            anim.speed = 0;
-            Application.OpenURL("https://twitter.com/RobertRowe1013");
-        }
-        else
-        {
-            isSpinning = true;
-            anim.speed = 1;
-        }
+        TopClick();
+        Application.OpenURL("https://twitter.com/RobertRowe1013");
     }
 }
